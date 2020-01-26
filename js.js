139d7499fd2f45ff53785902445e781f6cfa940d
Dakrus <dakrus17@gmail.com>
2020-01-26 16:00:19 +0200

    drone block

---
+++ js.js
@@ -0,0 +1,10 @@
+$(function(changeColor) {
+    $(window).scroll(function () {
+       if ($(this).scrollTop() > 500) {
+          $('nav').addClass('changeColor')
+       }
+       if ($(this).scrollTop() < 500) {
+          $('nav').removeClass('changeColor')
+       }
+    });
+ });
