53e1db389657207b0223d664ec2814070023aaed
Dakrus <dakrus17@gmail.com>
2020-01-27 13:40:29 +0200

    done without responsive

--- js.js
+++ js.js
@@ -1,10 +1,9 @@
-$(function(changeColor) {
-    $(window).scroll(function () {
-       if ($(this).scrollTop() > 500) {
-          $('nav').addClass('changeColor')
-       }
-       if ($(this).scrollTop() < 500) {
-          $('nav').removeClass('changeColor')
-       }
-    });
- });
+$(function() {
+   $(window).on("scroll", function(){
+      if($(window).scrollTop() > 150){
+         $("header").addClass("scrolled");
+      } else {
+         $("header").removeClass("scrolled");
+      }
+   });
+});
